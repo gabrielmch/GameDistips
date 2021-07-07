6f11d842a8c1534cfbc6dc3df44bdfd166a4de43
Gabriel Michaeli <GabrielMch04@hotmail.com>
2021-07-06 22:16:33 -0300

    Atualização do cabeçalho

--- components/cabecalho.js
+++ components/cabecalho.js
@@ -5,28 +5,19 @@ class cabecalhoNav extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-    <header class="cabecalho">
-      <nav>
-        <div class="cabecalho__nav container">
-          <a href="index.html" class="cabecalho__nav--font">GameDistips</a>
+    <header>
+      <nav class="cabecalho container">
+          <a href="index.html" class="cabecalho__logo">GameDistips</a>
           <div class="cabecalho__busca">
             <input type="text" placeholder="Buscar por Jogos" />
             <button class="cabecalho__busca--botao" type="submit"></button>
           </div>
-          <a href="login.html" class="cabecalho__nav--font">
-            Entrar
-            <img
-              src="/img/svg/acesso-icon.svg"
-              alt=""
-              class="cabecalho__nav--login"
-            />
-          </a>
-        </div>
-        <div class="cabecalho__links">
-          <a href="promocoes.html">Promoções</a>
-          <a href="#">Lista de Desejos</a>
-          <a href="plataformas.html">Plataformas</a>
-        </div>
+          <div class="cabecalho__links">
+            <a href="promocoes.html">Promoções</a>
+            <a href="#">Lista de Desejos</a>
+            <a href="plataformas.html">Plataformas</a>
+            <a href="login.html">Entrar</a>
+          </div>
       </nav>
     </header>
     `;
