d9fe3508be03cfbf5f78add9f9f1f66cf9f1d8c0
Gabriel Michaeli <51057760+gabrielmch@users.noreply.github.com>
2021-08-19 03:18:27 -0300

    pequenas alterações

--- components/conteudoHome-slides.js
+++
@@ -1,8 +0,0 @@
-let contador = 1;
-setInterval(function () {
-  document.getElementById("radioHome" + contador).checked = true;
-  contador++;
-  if (contador > 4) {
-    contador = 1;
-  }
-}, 4000);
